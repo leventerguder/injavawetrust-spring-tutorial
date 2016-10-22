29aea0b3eca15df621ecc300067d71b357e250bc
leventerguder <erguder.levent@gmail.com>
2016-10-22 16:15:32 +0300

    BeanFactory review

--- Injavawetrust.spring/src/_02/beanfactory/impl/test/BeanFactoryImplTest.java
+++ Injavawetrust.spring/src/_02/beanfactory/impl/test/BeanFactoryImplTest.java
@@ -9,16 +9,12 @@ import _02.beanfactory.impl.model.Car;
 public class BeanFactoryImplTest {
 
 	public static void main(String[] args) {
+		
 		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
 		XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(factory);
 		rdr.loadBeanDefinitions(new ClassPathResource("02.appcontext.xml"));
-
-		// Car car = (Car) factory.getBean(Car.class);
-		// <context:component-scan base-package="_02.beanfactory.impl.model" />
-		// kullanildiginda
-		// @Component olarak Car sinifimizda kullandigimiz icin otomatik olarak
-		// Car objesi olusturulur.
-
+		
+		
 		Car car = (Car) factory.getBean("carId");
 		System.out.println(car.getBrand());
 	}
